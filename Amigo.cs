1b74e8ce8188227b7ea3f1d12d00cccabe99d14a
francodasilva <69932299+francodasilva@users.noreply.github.com>
2022-07-05 18:19:08 +0200

    cambios de franco

---
+++ Amigo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase7
+{
+    public class Amigo
+    {
+        public int idAmigo { get; set; }
+       
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+       
+
+        public Amigo() { }
+        public Amigo(string _nombre, string _apellido)
+        {
+            
+            
+            this.nombre = _nombre;
+            this.apellido = _apellido;
+          
+        }
+     }
+}
---
+++ UsuarioAmigo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Clase7
+{
+    public class UsuarioAmigo
+    {
+        public int num_usr { get; set; }
+        [ForeignKey(nameof(num_usr))]
+        public Usuario user { get; set; }
+        public int num_usr2 { get; set; }
+        [ForeignKey(nameof(num_usr2))]
+        public Usuario amigo { get; set; }
+
+        public UsuarioAmigo() { }
+
+        public UsuarioAmigo(Usuario ppal, Usuario segundo)
+        {
+            user = ppal;
+            amigo = segundo;
+        }
+    }
+}
